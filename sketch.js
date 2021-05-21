baf5a42ca75ca60f9771a456137a926228440a56
Manuela <manuella23bermudez@gmail.com>
2021-05-21 17:19:37 -0500

    Agregar de nuevo pq no sirvio vscode

--- sketch.js
+++ sketch.js
@@ -8,9 +8,12 @@ function draw() {
   textAlign(CENTER);
   textSize(35);
   text('Manuela Bermudez', 200,200);
-  /*fill(255,0,0);
+  fill(255,180,0);
   ellipse(50,50,50,50);
 
-  fill(255,255,0);
-  ellipse(50,150,50,50);*/
+  fill(255,255,140);
+  ellipse(50,150,50,50);
+
+
+
 }
